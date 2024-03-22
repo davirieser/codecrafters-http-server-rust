01fdde22cc8dbb2d9e5fd736a2105992227c14f7
davirieser <david.w.rieser@gmail.com>
2024-03-22 20:19:30 +0100

    stage 2

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,27 @@ use std::net::TcpListener;
 use std::io::{Write, Read};
 
 fn main() {
+    const BUFFER_SIZE : usize = 1024;
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-                let mut buf : [u8; 1024] = [0; 1024];
+                let mut buf : [u8; BUFFER_SIZE] = [0; BUFFER_SIZE];
                 loop {
                     match _stream.read(&mut buf) {
                         Ok(0) => break,
-                        Ok(_n) => {},
-                        Err(e) => eprintln!("Error reading Data: {}", e)
+                        Ok(_n) => {
+                            println!("Read: {} Bytes", _n);
+                            if _n < BUFFER_SIZE {
+                                break;
+                            }
+                        },
+                        Err(e) => panic!("Error reading Data: {}", e)
                     }
                 }
 
-                let _ = write!(_stream, "HTTP/1.1 200 OK\r\n\r\n");
+                let _ = _stream.write(&mut "HTTP/1.1 200 OK\r\n\r\n".as_bytes());
             }
             Err(e) => {
                 println!("error: {}", e);
