90cb92eb23a5cabd5be64a0ff4ba03a7444d7906
davirieser <david.w.rieser@gmail.com>
2024-03-22 22:26:30 +0100

    stage 7

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,27 @@ use std::env;
 use std::sync::Arc;
 use std::path::PathBuf;
 use std::fs;
+use std::convert::TryFrom;
+
+#[derive(PartialEq, Eq, Debug)]
+enum HttpMethod {
+    GET,
+    POST,
+    PUT,
+}
+
+impl TryFrom<&str> for HttpMethod {
+    type Error = ();
+
+    fn try_from(s: &str) -> Result<Self, Self::Error> {
+        match s {
+            "GET" => Ok(HttpMethod::GET),
+            "POST" => Ok(HttpMethod::POST),
+            "PUT" => Ok(HttpMethod::PUT),
+            _ => Err(()),
+        }
+    } 
+}
 
 fn split_header(header: &str) -> Option<(&str, &str)> {
     let mut iter = header.splitn(2, ':');
@@ -38,6 +59,46 @@ fn read_to_string<R: Read>(stream: &mut R) -> Option<String> {
     String::from_utf8(vec).ok()
 }
 
+fn send_file(stream: &mut TcpStream, path: &str, dir: &str) {
+    let file_name = path.strip_prefix("/files/").unwrap();
+    let absolute_path = format!("{dir}{file_name}");
+    
+    let file_path = PathBuf::from(absolute_path);
+
+    if !file_path.exists() || !file_path.is_file() {
+        let _ = write!(stream, "HTTP/1.1 404 Not Found\r\n\r\n");
+    } else {
+        match fs::read(file_path) {
+            Ok(contents) => {
+                let len = contents.len();
+
+                let _ = write!(stream, "HTTP/1.1 200 OK\r\n");
+                let _ = write!(stream, "Content-Type: application/octet-stream\r\n");
+                let _ = write!(stream, "Content-Length: {len}\r\n\r\n");
+
+                let _ = stream.write(&contents);
+            }
+            Err(e) => panic!("Could not open File: {e}"),
+        }
+    }
+}
+
+fn save_file(stream: &mut TcpStream, path: &str, dir: &str, body: String) {
+    let file_name = path.strip_prefix("/files/").unwrap();
+    let absolute_path = format!("{dir}{file_name}");
+    
+    let file_path = PathBuf::from(absolute_path);
+
+    match fs::write(file_path, body) {
+        Ok(()) => {
+            let _ = write!(stream, "HTTP/1.1 201 Created\r\n\r\n");
+        }
+        Err(_) => {
+            let _ = write!(stream, "HTTP/1.1 500 Internal Server Error\r\n\r\n");
+        }
+    }
+}
+
 fn handle_connection(stream: &mut TcpStream, dir: Arc<Option<String>>) {
     match read_to_string(stream) {
         Some(buf) => {
@@ -49,8 +110,12 @@ fn handle_connection(stream: &mut TcpStream, dir: Arc<Option<String>>) {
                 },
                 None => panic!("Empty Request!")
             };
+            let method = HttpMethod::try_from(method).expect("Error parsing HTTP Method");
 
-            let headers : Vec<(&str, &str)> = lines.filter_map(split_header).collect();
+            let headers = lines.by_ref().take_while(|line| line.is_empty());
+            let headers : Vec<(&str, &str)> = headers.filter_map(split_header).collect();
+
+            let body = lines.fold(String::new(), |a, b| a + b + "\n"); 
 
             match path {
                 "/" => {
@@ -85,26 +150,10 @@ fn handle_connection(stream: &mut TcpStream, dir: Arc<Option<String>>) {
                 _ if path.starts_with("/files/") => {
                     match dir.as_ref() {
                         Some(dir) => {
-                            let file_name = path.strip_prefix("/files/").unwrap();
-                            let absolute_path = format!("{dir}{file_name}");
-                            
-                            let file_path = PathBuf::from(absolute_path);
-
-                            if !file_path.exists() || !file_path.is_file() {
-                                let _ = write!(stream, "HTTP/1.1 404 Not Found\r\n\r\n");
-                            } else {
-                                match fs::read(file_path) {
-                                    Ok(contents) => {
-                                        let len = contents.len();
-
-                                        let _ = write!(stream, "HTTP/1.1 200 OK\r\n");
-                                        let _ = write!(stream, "Content-Type: application/octet-stream\r\n");
-                                        let _ = write!(stream, "Content-Length: {len}\r\n\r\n");
-
-                                        let _ = stream.write(&contents);
-                                    }
-                                    Err(e) => panic!("Could not open File: {e}"),
-                                }
+                            if method == HttpMethod::GET {
+                                send_file(stream, path, dir);
+                            } else if method == HttpMethod::POST {
+                                save_file(stream, path, dir, body);
                             }
                         }
                         None => {
@@ -123,7 +172,7 @@ fn handle_connection(stream: &mut TcpStream, dir: Arc<Option<String>>) {
 
 fn main() {
     let args: Vec<_> = env::args().collect();
-    let mut dir = args
+    let dir = args
         .iter()
         .position(|arg| arg == "--directory")
         .and_then(|idx| args.get(idx + 1).cloned())
