b1a31e554dadbe75f818b90983611dc9cbc89a87
davirieser <david.w.rieser@gmail.com>
2024-03-24 00:21:18 +0100

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,203 @@
-use std::net::{TcpListener, TcpStream};
-use std::io::{Write, Read};
-use std::thread;
+use std::io;
 use std::env;
 use std::sync::Arc;
 use std::path::PathBuf;
-use std::fs;
 use std::convert::TryFrom;
+use std::collections::HashMap;
+use std::fmt::{Debug, Display};
+
+use anyhow::{Error, Result};
+
+use tokio::net::{TcpListener, TcpStream};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::signal;
+use tokio::fs::File;
+
+#[derive(Debug)]
+struct Request<'a> {
+    method: HttpMethod,
+    path: &'a str,
+    http_version: &'a str,
+    headers: HashMap<&'a str, HeaderValue<'a>>, 
+    body: &'a str,
+}
+
+struct Response {
+    status_code: HttpStatusCode,
+    headers: Vec<(String, String)>,
+    body: String,
+}
+
+impl Response {
+    pub fn new(status_code: HttpStatusCode, headers: Vec<(String, String)>, body: String) -> Self {
+        Response {
+            status_code,
+            headers,
+            body,
+        }
+    }
+    pub fn new_without_body(status_code: HttpStatusCode, headers: Vec<(String, String)>) -> Self {
+        Response {
+            status_code,
+            headers,
+            body: String::with_capacity(0),
+        }
+    }
+    pub async fn write_to<W>(&self, w: &mut W) -> io::Result<usize>
+    where 
+        W: AsyncWriteExt + Unpin
+    {
+        let status_code_int = usize::from(self.status_code);
+        let mut buf = format!("HTTP/1.1 {status_code_int} {}\r\n", self.status_code);
+
+        for (key, value) in &self.headers {
+            buf += key;
+            buf += ": ";
+            buf += value;
+            buf += "\r\n";
+        }
+
+        buf += "\r\n";
+
+        buf += &self.body;
+
+        w.write(buf.as_bytes()).await
+    }
+}
+
+impl From<HttpStatusCode> for Response {
+    fn from(sc: HttpStatusCode) -> Self {
+        Response::new(sc, Vec::with_capacity(0), String::with_capacity(0))
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum HeaderValue<'a> {
+    Single(&'a str),
+    Multiple(Vec<&'a str>),
+}
 
 #[derive(PartialEq, Eq, Debug)]
 enum HttpMethod {
     GET,
     POST,
     PUT,
+    DELETE,
+    PATCH,
+    HEAD,
+    OPTIONS,
+    TRACE,
+    CONNECT,
+}
+
+impl Display for HttpMethod {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+enum HttpStatusCode {
+    Ok,
+    Created,
+
+    BadRequest,
+    NotFound,
+
+    InternalServerError,
+}
+
+impl From<HttpStatusCode> for usize {
+    fn from(sc: HttpStatusCode) -> usize {
+        match sc {
+            HttpStatusCode::Ok => 200,
+            HttpStatusCode::Created => 201,
+            HttpStatusCode::BadRequest => 400,
+            HttpStatusCode::NotFound => 404,
+            HttpStatusCode::InternalServerError => 500,
+        }
+    }
+}
+
+impl Display for HttpStatusCode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let str = match self {
+            Self::Ok => "OK",
+            Self::Created => "Created",
+            Self::BadRequest => "Bad Request",
+            Self::NotFound => "Not Found",
+            Self::InternalServerError => "Internal Server Error",
+        };
+
+        write!(f, "{str}")
+    }
 }
 
 impl TryFrom<&str> for HttpMethod {
     type Error = ();
 
-    fn try_from(s: &str) -> Result<Self, Self::Error> {
-        match s {
+    fn try_from(method: &str) -> Result<Self, Self::Error> {
+        match method.to_uppercase().as_str() {
             "GET" => Ok(HttpMethod::GET),
             "POST" => Ok(HttpMethod::POST),
             "PUT" => Ok(HttpMethod::PUT),
+            "DELETE" => Ok(HttpMethod::DELETE),
+            "PATCH" => Ok(HttpMethod::PATCH),
+            "HEAD" => Ok(HttpMethod::HEAD),
+            "OPTIONS" => Ok(HttpMethod::OPTIONS),
+            "TRACE" => Ok(HttpMethod::TRACE),
+            "CONNECT" => Ok(HttpMethod::CONNECT),
             _ => Err(()),
         }
     } 
+}
+
+#[derive(Debug)]
+enum RouteError {
+    NoMatch,
+    Error(Error),
+}
+
+impl std::error::Error for RouteError {}
+
+impl Display for RouteError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            RouteError::NoMatch => write!(f, "Route did not match Path!"),
+            RouteError::Error(e) => write!(f, "Internal Error: {e}"),
+        }
+    }
+}
+
+struct RouteDefinition<T, M, A>
+where 
+    M: Fn(&str) -> Option<T>,
+    A: Fn(Request, T) -> Result<Response>,
+{
+    matches: M,
+    action: A,
+}
+
+struct Route
+{
+    run: Box<dyn Fn(Request<'_>) -> Result<Response>>,
+}
+
+impl<T, M, A> From<RouteDefinition<T, M, A>> for Route
+where
+    M: Fn(&str) -> Option<T> + 'static,
+    A: Fn(Request, T) -> Result<Response> + 'static,
+{
+    fn from(definition: RouteDefinition<T, M, A>) -> Self {
+        let run = Box::new(move |request: Request| {
+            match (definition.matches)(request.path) {
+                Some(matches) => (definition.action)(request, matches),
+                None => Err(Error::new(RouteError::NoMatch)),
+            }
+        });
+
+        Self { run }
+    }
 }
 
 fn split_header(header: &str) -> Option<(&str, &str)> {
@@ -36,143 +209,158 @@ fn split_header(header: &str) -> Option<(&str, &str)> {
     Some((key, value))
 }
 
-fn read_to_string<R: Read>(stream: &mut R) -> Option<String> {
+async fn read_to_string<R: AsyncReadExt + std::marker::Unpin>(stream: &mut R) -> Result<String> {
     const BUFFER_SIZE : usize = 1024;
 
     let mut buf = [0 as u8; 1024];
     let mut vec = Vec::new();
 
     loop {
-        match stream.read(&mut buf) {
+        match stream.read(&mut buf).await {
             Ok(n) => {
                 vec.extend_from_slice(&buf[..n]);
                 if n < BUFFER_SIZE {
-                    break;
+                    return Ok(String::from_utf8(vec)?);
                 }
             }
             Err(e) => {
-                return None;
+                return Err(e.into());
             }
         }
     }
-
-    String::from_utf8(vec).ok()
 }
 
-fn send_file(stream: &mut TcpStream, path: &str, dir: &str) {
+async fn send_file(stream: &mut TcpStream, path: &str, dir: &str) -> io::Result<u64> {
     let file_name = path.strip_prefix("/files/").unwrap();
     let absolute_path = format!("{dir}{file_name}");
     
     let file_path = PathBuf::from(absolute_path);
 
-    if !file_path.exists() || !file_path.is_file() {
-        let _ = write!(stream, "HTTP/1.1 404 Not Found\r\n\r\n");
-    } else {
-        match fs::read(file_path) {
-            Ok(contents) => {
-                let len = contents.len();
+    let mut file = File::open(file_path).await?;
+    let size = file.metadata().await?.len();
+    
+    let response = Response::new_without_body(
+        HttpStatusCode::Ok, 
+        vec![
+            ("Content-Type".to_string(), "application/octet-stream".to_string()),
+            ("Content-Length".to_string(), format!("{size}"))
+        ],
+    );
 
-                let _ = write!(stream, "HTTP/1.1 200 OK\r\n");
-                let _ = write!(stream, "Content-Type: application/octet-stream\r\n");
-                let _ = write!(stream, "Content-Length: {len}\r\n\r\n");
-
-                let _ = stream.write(&contents);
-            }
-            Err(e) => panic!("Could not open File: {e}"),
-        }
-    }
+    response.write_to(stream).await?;
+    tokio::io::copy(&mut file, stream).await
 }
 
-fn save_file(stream: &mut TcpStream, path: &str, dir: &str, body: String) {
+async fn save_file(stream: &mut TcpStream, path: &str, dir: &str, body: String) -> io::Result<usize> {
     let file_name = path.strip_prefix("/files/").unwrap();
     let absolute_path = format!("{dir}{file_name}");
     
     let file_path = PathBuf::from(absolute_path);
 
-    match fs::write(file_path, body) {
-        Ok(()) => {
-            let _ = write!(stream, "HTTP/1.1 201 Created\r\n\r\n");
-        }
-        Err(_) => {
-            let _ = write!(stream, "HTTP/1.1 500 Internal Server Error\r\n\r\n");
-        }
-    }
+    let mut file = File::open(file_path).await?;
+    let res = file.write(body.as_bytes()).await;
+    Response::from(res
+        .as_ref()
+        .map_or_else(|_e| HttpStatusCode::InternalServerError, |_v| HttpStatusCode::Ok)
+    ).write_to(stream).await?;
+        
+    res
 }
 
-fn handle_connection(stream: &mut TcpStream, dir: Arc<Option<String>>) {
-    match read_to_string(stream) {
-        Some(buf) => {
+async fn handle_connection(mut stream: TcpStream, dir: Arc<Option<String>>) {
+    match read_to_string(&mut stream).await {
+        Ok(buf) => {
             let mut lines = buf.lines();
-            let (method, path, version) = match lines.next() {
-                Some(line) => {
-                    let parts : Vec<&str> = line.splitn(3, ' ').collect();
-                    (parts[0], parts[1], parts[2])
-                },
-                None => panic!("Empty Request!")
+            let header_line = lines.next()
+                .and_then(|line| line.find(' ').and_then(|i| Some((line, i))))
+                .and_then(|(line, idx1)| match (idx1, line.rfind(' ')) {
+                    (idx1, Some(idx2)) if idx1 != idx2 => Some((&line[..idx1], &line[idx1+1..idx2], &line[idx2+1..])),
+                    (_, _) => None,
+                });
+            let (method, path, version) = match header_line {
+                Some(t) => t,
+                None => {
+                    println!("Invalid Request");
+                    Response::from(HttpStatusCode::BadRequest).write_to(&mut stream).await;
+                    panic!("");
+                }
             };
+
             let method = HttpMethod::try_from(method).expect("Error parsing HTTP Method");
+
+            println!("Method: {method}, Path: {path}, Version: {version}");
 
             let headers = lines.by_ref().take_while(|line| !line.is_empty());
             let headers : Vec<(&str, &str)> = headers.filter_map(split_header).collect();
 
             let mut body = lines.fold(String::new(), |a, b| a + b + "\n"); 
-            // NOTE: Remove last newline that is inserted by the fold.
-            body.truncate(body.len() - 1);
+            if !body.is_empty() {
+                // NOTE: Remove last newline that is inserted by the fold.
+                body.truncate(body.len() - 1);
+            }
 
             match path {
                 "/" => {
-                    let _ = write!(stream, "HTTP/1.1 200 OK\r\n\r\n");
+                    Response::from(HttpStatusCode::Ok).write_to(&mut stream).await;
                 },
                 "/user-agent" => {
                     match headers.iter().find(|(key, _)| *key == "User-Agent") {
                         Some((_, user_agent)) => {
-                            let len = user_agent.len();
+                            let len = format!("{}", user_agent.len());
 
-                            let _ = write!(stream, "HTTP/1.1 200 OK\r\n");
-                            let _ = write!(stream, "Content-Type: text/plain\r\n");
-                            let _ = write!(stream, "Content-Length: {len}\r\n");
+                            let response = Response::new(
+                                HttpStatusCode::Ok,
+                                vec![
+                                    ("Content-Type".to_string(), "text/plain".to_string()),
+                                    ("Content-Length".to_string(), len),
+                                ],
+                                user_agent.to_string()
+                            );
 
-                            let _ = write!(stream, "\r\n{user_agent}");
+                            response.write_to(&mut stream).await;
                         }
                         None => {
-                            let _ = write!(stream, "HTTP/1.1 404 Not Found\r\n\r\n");
+                            Response::from(HttpStatusCode::NotFound).write_to(&mut stream).await;
                         }
                     }
                 }
                 _ if path.starts_with("/echo/") => {
                     let message = path.strip_prefix("/echo/").unwrap();
-                    let len = message.len();
+                    let len = format!("{}", message.len());
 
-                    let _ = write!(stream, "HTTP/1.1 200 OK\r\n");
-                    let _ = write!(stream, "Content-Type: text/plain\r\n");
-                    let _ = write!(stream, "Content-Length: {len}\r\n");
-
-                    let _ = write!(stream, "\r\n{message}");
+                    let response = Response::new(
+                        HttpStatusCode::Ok,
+                        vec![
+                            ("Content-Type".to_string(), "text/plain".to_string()),
+                            ("Content-Length".to_string(), len),
+                        ],
+                        message.to_string()
+                    );
                 }
                 _ if path.starts_with("/files/") => {
                     match dir.as_ref() {
                         Some(dir) => {
                             if method == HttpMethod::GET {
-                                send_file(stream, path, dir);
+                                send_file(&mut stream, path, dir);
                             } else if method == HttpMethod::POST {
-                                save_file(stream, path, dir, body);
+                                save_file(&mut stream, path, dir, body);
                             }
                         }
                         None => {
-                            let _ = write!(stream, "HTTP/1.1 404 Not Found\r\n\r\n");
+                            Response::from(HttpStatusCode::NotFound).write_to(&mut stream).await;
                         }
                     }
                 }
                 _ => {
-                    let _ = write!(stream, "HTTP/1.1 404 Not Found\r\n\r\n");
+                    Response::from(HttpStatusCode::NotFound).write_to(&mut stream).await;
                 }
             }
         },
-        None => panic!("Error reading Data"),
+        Err(e) => println!("Error reading Data: {e}"),
     }
 }
 
-fn main() {
+async fn main_loop() -> io::Result<()> {
     let args: Vec<_> = env::args().collect();
     let dir = args
         .iter()
@@ -188,23 +376,23 @@ fn main() {
 
     let arc = Arc::new(dir);
 
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    
-    let mut handles = vec![];
+    let listener = TcpListener::bind("127.0.0.1:4221").await.unwrap();
 
-    for stream in listener.incoming() {
+    loop {
+        let (socket, socket_addr) = listener.accept().await?;
+        
+        println!("New Connection from {socket_addr}");
+
         let dir_ref = arc.clone();
-        match stream {
-            Ok(mut stream) => {
-                handles.push(thread::spawn(move || handle_connection(&mut stream, dir_ref)));
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        handle_connection(socket, dir_ref).await;
     }
+}
 
-    for handle in handles {
-        let _ = handle.join();
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    tokio::select! {
+        biased;
+        v = main_loop() => v,
+        v = signal::ctrl_c() => v,
     }
 }
